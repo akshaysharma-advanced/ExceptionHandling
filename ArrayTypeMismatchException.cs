3b4e99a33b433d971eaa4dc065578ba94c97d4dc
Akshay.Sharma <akshay.sharma@oneadvanced.com>
2022-11-04 10:37:54 +0530

    Add project files.

---
+++ ArrayTypeMismatchException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling_Assignment
+{
+    //Creating class for ArrayTypeMismatchException
+    internal class TypeMismatchException
+    {
+        public TypeMismatchException()
+        {
+            try
+            {
+                //Create string array fruits 
+                string[] fruits = { "Apple", "Banana", "Orange", "Mango", "Kiwi" };
+
+                //Creat int array price
+                int[] price = { 100, 50, 300, 900, 250 };
+
+                // Copying fruits array into price array
+                Array.Copy(fruits, price, 2);
+            }
+            catch (ArrayTypeMismatchException AtmExc)
+            {
+                Console.WriteLine("ArrayTypeMismatchException has occured here");
+                //print Exception Message
+                Console.WriteLine(AtmExc.Message);
+            }
+        }
+    }
+}
