5b3950f94fe0a67d8efb140208a9e7880f20cc80
Akshay.Sharma <akshay.sharma@oneadvanced.com>
2022-11-04 10:51:57 +0530

    Edit Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Object for ArrayTypeMismatchExeption
+            TypeMismatchException typeMismatchException = new TypeMismatchException();
+
+            Console.WriteLine("------------------------------------------------------------------------");
+
+            // Object for DivideByZeroException
+            DivByZeroException divbyzeroException = new DivByZeroException();
+
+            Console.WriteLine("------------------------------------------------------------------------");
+
+            // Object for InvalidCastingException
+            CastingException castException = new CastingException();
+
+            Console.WriteLine("------------------------------------------------------------------------");
+
+            // Object for IndexOutOfRangeException
+            OutOfRangeException outOfRangeException = new OutOfRangeException();
+
+            Console.WriteLine("------------------------------------------------------------------------");
+
+            // Object for IOException
+            ExceptionIO exceptionIO = new ExceptionIO();
+
+            Console.WriteLine("------------------------------------------------------------------------");
+
+            // Object for NullReferenceException
+            NullPointException nullPointException = new NullPointException();
+
+            Console.WriteLine("------------------------------------------------------------------------");
+
+            // Object for StackOverflowException
+            StOverflowException overflowException = new StOverflowException();
+
+            Console.WriteLine("------------------------------------------------------------------------");
+        }
+    }
+}
